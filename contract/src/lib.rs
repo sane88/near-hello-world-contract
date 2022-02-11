c35770bb68a0175435f904d32951439bec679558
Valentyn Riabokonenko <sane.88@gmail.com>
2022-02-11 14:28:29 +0000

    Contract with simple test

---
+++ contract/src/lib.rs
@@ -0,0 +1,25 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{near_bindgen};
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct HelloWorldContract {
+}
+
+#[near_bindgen]
+impl HelloWorldContract {
+
+    pub fn hello_world(name: &str) -> String {
+        return format!("Hello {}!", name)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn say_hello() {
+        assert_eq!("Hello Chuck!", HelloWorldContract::hello_world("Chuck"))
+    }
+}
